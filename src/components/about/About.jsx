1d6950c1eb33fd550167b0cb24d5b3ae2393adbd
bayakami <90577236+bayakami@users.noreply.github.com>
2021-11-20 16:08:34 +0000

    about me section added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,30 @@
-import "./about.css";
+import "./about.css"
+import Background from "../../img/bg-img.jpg"
 
 const About = () => {
     return (
-        <div className="a">
-            about
+      <div className="a">
+        <div className="a-left">
+          <div className="a-card bg"></div>
+          <div className="a-card">
+          <img src={Background} alt="" className="a-img" />
+          </div>
         </div>
+        <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-sub">
+          It is a long established fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <p className="a-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+        </p>
+          
+        </div>
+      </div>
     )
 }
 
