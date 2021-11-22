394ebaeb2ffc876b01b911dcd41b34d85d1d3347
bayakami <90577236+bayakami@users.noreply.github.com>
2021-11-22 15:58:23 +0000

    screenshots of projects added

--- src/data.js
+++ src/data.js
@@ -1,32 +1,32 @@
 export const projects = [
     {
       id: 1,
-      img: "../../img/image1.jpg",
+      img: "https://github.com/bayakami/Portfolio-Updated/blob/main/src/img/image1.jpeg?raw=true",
       link: "https://bayakami.github.io/Week-4-Project-Ceaser/",
     },
     {
       id: 2,
       img: "https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg",
-      link: "http://lama.dev",
+      link: "https://bayakami.github.io/week2-project/",
     },
     {
       id: 3,
       img: "https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg",
-      link: "http://lama.dev",
+      link: "https://bayakami.github.io/week-3-OOP-game/",
     },
     {
       id: 4,
       img: "https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg",
-      link: "http://lama.dev",
+      link: "https://github.com/bayakami/weather-app",
     },
     {
       id: 5,
       img: "https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg",
-      link: "http://lama.dev",
+      link: "",
     },
     {
       id: 6,
       img: "https://images.unsplash.com/photo-1585141239600-03449aab6e10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      link: "http://lama.dev",
+      link: "",
     },
   ];
